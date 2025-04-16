c30c0918d3100101891de8c995075a428613750f
Miguel Matos <78162554+MiguelMatos98@users.noreply.github.com>
2025-04-16 18:56:01 +0100

    Update Inventory.h

--- Source/UEInventory/Public/Inventory.h
+++ Source/UEInventory/Public/Inventory.h
@@ -29,39 +29,15 @@
 UENUM(BlueprintType)
 enum class EDirection : uint8
 {
+    None,
     Up,
     Down,
     Left,
-    Right,
-    None = 255
-};
-
-USTRUCT()
-struct FSlideData
-{
-    GENERATED_BODY()
-
-    TObjectPtr<UOverlay> WidgetA;
-    TObjectPtr<UOverlay> WidgetB;
-    FVector2D StartPosA;
-    FVector2D StartPosB;
-    FVector2D EndPosA;
-    FVector2D EndPosB;
-    uint64 IndexA;
-    uint64 IndexB;
-    float ElapsedTime;
-
-    FSlideData()
-        : WidgetA(nullptr), WidgetB(nullptr),
-        StartPosA(FVector2D::ZeroVector), StartPosB(FVector2D::ZeroVector),
-        EndPosA(FVector2D::ZeroVector), EndPosB(FVector2D::ZeroVector),
-        IndexA(0), IndexB(0), ElapsedTime(0.0f)
-    {
-    }
+    Right
 };
 
 UCLASS()
-class UEINVENTORY_API UInventory : public UUserWidget
+class UInventory : public UUserWidget
 {
     GENERATED_BODY()
 
@@ -70,12 +46,13 @@ public:
 
     virtual void NativeOnInitialized() override;
     virtual void NativeConstruct() override;
-    virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
     UFUNCTION(BlueprintCallable, Category = "Inventory")
     void AddItem(AActor* ItemActor);
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    void RemoveItem();
 
     UFUNCTION(BlueprintCallable, Category = "Inventory")
     void Open();
@@ -95,24 +72,32 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Inventory")
     UUniformGridPanel* GetGrid();
 
-    uint64 FindHoveredItemIndex(const FPointerEvent& InMouseEvent);
     void MoveItem(const FPointerEvent& MouseEvent, bool bItemMovementStarted, bool bItemMovementFinished);
-
+    uint64 FindHoveredItemIndex(const FPointerEvent& InMouseEvent);
 protected:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
-    int32 MaxRows = 3;
+    virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
-    int32 MaxColumns = 4;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
-    UWidgetAnimation* SlideSwap;
+    void Create();
+    void CreateItemIcon(uint64 SlotIndex);
+    void CreateIconCounterText(uint64 SlotIndex);
+    uint64 FindFirstEmptySlot() const;
+    UOverlay* FindDraggedOverlay(uint64 ItemIndex);
+    UOverlay* CreateTemporaryDragOverlay(const FItem& Item);
+    EDirection GetMoveDirection(uint64 RowA, uint64 ColA, uint64 RowB, uint64 ColB);
+    EDirection SortItem(FItem& MovedItem, FItem& ItemToMove);
+    int32 FindItemIndex(const FItem& TargetItem) const;
+    void UpdateSlotUI(uint64 SlotIndex);
+    void RemoveItemIcon(uint64 SlotIndex);
+    void ShiftItems(uint64 StartIndex, uint64 EndIndex, EDirection Direction, bool bUpdateUI = true);
+    void StartSlideAnimation(uint64 FromIndex, uint64 ToIndex, EDirection Direction);
+    FVector2D GetSlotPosition(uint64 SlotIndex) const;
+    static float CustomEaseInOut(float T);
 
     UPROPERTY()
     TObjectPtr<UCanvasPanel> Canvas;
-
-    UPROPERTY()
-    TObjectPtr<UCanvasPanelSlot> BackgroundBorderSlot;
 
     UPROPERTY()
     TObjectPtr<UBorder> BackgroundBorder;
@@ -124,16 +109,28 @@ protected:
     TObjectPtr<UUniformGridPanel> Grid;
 
     UPROPERTY()
-    TObjectPtr<UUniformGridSlot> GridSlot;
+    UCanvasPanelSlot* BackgroundBorderSlot;
+
+    UPROPERTY()
+    UUniformGridSlot* GridSlot;
 
     UPROPERTY()
     TArray<TObjectPtr<UBorder>> ForegroundBorders;
 
     UPROPERTY()
+    TArray<TObjectPtr<UOverlay>> IconSlots;
+
+    UPROPERTY()
     TArray<FItem> Items;
 
     UPROPERTY()
-    TArray<TObjectPtr<USizeBox>> IconSlots;
+    TArray<bool> bCounterTextUpdated;
+
+    UPROPERTY()
+    uint64 MaxRows = 3;
+
+    UPROPERTY()
+    uint64 MaxColumns = 4;
 
     UPROPERTY()
     bool bIsInventoryFull;
@@ -148,24 +145,38 @@ protected:
     TObjectPtr<UOverlay> DraggedOverlay;
 
     UPROPERTY()
-    TArray<bool> bCounterTextUpdated;
+    bool bIsDragging;
 
     UPROPERTY()
-    FSlideData CurrentSlideData;
+    bool bIsSliding;
+
+    UPROPERTY()
+    uint64 SlideFromIndex;
+
+    UPROPERTY()
+    uint64 SlideToIndex;
+
+    UPROPERTY()
+    float SlideProgress;
+
+    UPROPERTY()
+    float SlideDuration;
+
+    UPROPERTY()
+    FItem SlidingItem;
+
+    // Added to support multi-item sliding
+    UPROPERTY()
+    TArray<TObjectPtr<UOverlay>> SlidingOverlays;
+
+    UPROPERTY()
+    TArray<uint64> SlideFromIndices;
+
+    UPROPERTY()
+    TArray<uint64> SlideToIndices;
+
+    UPROPERTY()
+    TArray<FItem> SlidingItems;
 
     static uint64 ItemCounter;
-
-    void Create();
-    void CreateItemIcon(uint64 SlotIndex);
-    void CreateIconCounterText(uint64 SlotIndex);
-    uint64 FindFirstEmptySlot() const;
-    TObjectPtr<UOverlay> FindDraggedOverlay(uint64 ItemIndex);
-    EDirection GetMoveDirection(uint64 RowA, uint64 ColA, uint64 RowB, uint64 ColB);
-    EDirection SortItem(FItem& MovedItem, FItem& ItemToMove);
-    int32 FindItemIndex(const FItem& TargetItem) const;
-    void RemoveItem();
-    void UpdateSlideTick();
-    void UpdateSlotUI(uint64 SlotIndex);
-    void RemoveItemIcon(uint64 SlotIndex);
-    void ShiftItems(uint64 FromIndex, uint64 ToIndex);
 };
