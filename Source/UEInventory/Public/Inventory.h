629fd8e2fc6995d0f9f65220b5d90a5fa0e3db74
Miguel Matos <78162554+MiguelMatos98@users.noreply.github.com>
2025-04-17 16:09:23 +0100

    Update Inventory.h

--- Source/UEInventory/Public/Inventory.h
+++ Source/UEInventory/Public/Inventory.h
@@ -10,7 +10,6 @@
 #include "Components/VerticalBoxSlot.h"
 #include "Components/TextBlock.h"
 #include "Components/Border.h"
-#include "UEInventory/Item.h"
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 #include "Engine/Engine.h"
@@ -24,6 +23,7 @@
 #include "Misc/Paths.h"
 #include "Engine/Texture2D.h"
 #include "Components/SizeBox.h"
+#include "Item.h"
 #include "Inventory.generated.h"
 
 UENUM(BlueprintType)
@@ -44,10 +44,18 @@ class UInventory : public UUserWidget
 public:
     UInventory(const FObjectInitializer& ObjectInitializer);
 
+    static uint64 ItemCounter;
+
+protected:
     virtual void NativeOnInitialized() override;
     virtual void NativeConstruct() override;
     virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+    virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+    virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 
+public:
     UFUNCTION(BlueprintCallable, Category = "Inventory")
     void AddItem(AActor* ItemActor);
 
@@ -74,13 +82,8 @@ public:
 
     void MoveItem(const FPointerEvent& MouseEvent, bool bItemMovementStarted, bool bItemMovementFinished);
     uint64 FindHoveredItemIndex(const FPointerEvent& InMouseEvent);
-protected:
-    virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 
-    void Create();
+protected:
     void CreateItemIcon(uint64 SlotIndex);
     void CreateIconCounterText(uint64 SlotIndex);
     uint64 FindFirstEmptySlot() const;
@@ -91,41 +94,15 @@ protected:
     int32 FindItemIndex(const FItem& TargetItem) const;
     void UpdateSlotUI(uint64 SlotIndex);
     void RemoveItemIcon(uint64 SlotIndex);
-    void ShiftItems(uint64 StartIndex, uint64 EndIndex, EDirection Direction, bool bUpdateUI = true);
+    void ShiftItems(uint64 StartIndex, uint64 EndIndex, EDirection Direction, bool bUpdateUI);
     void StartSlideAnimation(uint64 FromIndex, uint64 ToIndex, EDirection Direction);
     FVector2D GetSlotPosition(uint64 SlotIndex) const;
-    static float CustomEaseInOut(float T);
+    float CustomEaseInOut(float T);
+    void Create();
 
-    UPROPERTY()
-    TObjectPtr<UCanvasPanel> Canvas;
+    void ScheduleSlideAnimation(uint64 FromIndex, uint64 ToIndex, EDirection Direction);
 
-    UPROPERTY()
-    TObjectPtr<UBorder> BackgroundBorder;
-
-    UPROPERTY()
-    TObjectPtr<UTextBlock> Title;
-
-    UPROPERTY()
-    TObjectPtr<UUniformGridPanel> Grid;
-
-    UPROPERTY()
-    UCanvasPanelSlot* BackgroundBorderSlot;
-
-    UPROPERTY()
-    UUniformGridSlot* GridSlot;
-
-    UPROPERTY()
-    TArray<TObjectPtr<UBorder>> ForegroundBorders;
-
-    UPROPERTY()
-    TArray<TObjectPtr<UOverlay>> IconSlots;
-
-    UPROPERTY()
-    TArray<FItem> Items;
-
-    UPROPERTY()
-    TArray<bool> bCounterTextUpdated;
-
+private:
     UPROPERTY()
     uint64 MaxRows = 3;
 
@@ -136,7 +113,43 @@ protected:
     bool bIsInventoryFull;
 
     UPROPERTY()
-    uint64 DraggedItemIndex;
+    TArray<FItem> Items;
+
+    UPROPERTY()
+    TArray<TObjectPtr<UBorder>> ForegroundBorders;
+
+    UPROPERTY()
+    TArray<TObjectPtr<UOverlay>> IconSlots;
+
+    UPROPERTY()
+    TArray<bool> bCounterTextUpdated;
+
+    UPROPERTY()
+    TObjectPtr<UCanvasPanel> Canvas;
+
+    UPROPERTY()
+    TObjectPtr<UBorder> BackgroundBorder;
+
+    UPROPERTY()
+    UCanvasPanelSlot* BackgroundBorderSlot;
+
+    UPROPERTY()
+    TObjectPtr<UTextBlock> Title;
+
+    UPROPERTY()
+    TObjectPtr<UUniformGridPanel> Grid;
+
+    UPROPERTY()
+    UUniformGridSlot* GridSlot;
+
+    UPROPERTY()
+    int32 DraggedItemIndex;
+
+    UPROPERTY()
+    int32 OriginalSlotIndex;
+
+    UPROPERTY()
+    int32 PreviousSlotIndex; // Added to track the previous slot during dragging
 
     UPROPERTY()
     FItem DraggedItem;
@@ -151,10 +164,10 @@ protected:
     bool bIsSliding;
 
     UPROPERTY()
-    uint64 SlideFromIndex;
+    int32 SlideFromIndex;
 
     UPROPERTY()
-    uint64 SlideToIndex;
+    int32 SlideToIndex;
 
     UPROPERTY()
     float SlideProgress;
@@ -165,18 +178,30 @@ protected:
     UPROPERTY()
     FItem SlidingItem;
 
-    // Added to support multi-item sliding
     UPROPERTY()
     TArray<TObjectPtr<UOverlay>> SlidingOverlays;
 
     UPROPERTY()
-    TArray<uint64> SlideFromIndices;
+    TArray<int32> SlideFromIndices;
 
     UPROPERTY()
-    TArray<uint64> SlideToIndices;
+    TArray<int32> SlideToIndices;
 
     UPROPERTY()
     TArray<FItem> SlidingItems;
 
-    static uint64 ItemCounter;
+    UPROPERTY()
+    bool bAnimationScheduled;
+
+    UPROPERTY()
+    uint64 ScheduledFromIndex;
+
+    UPROPERTY()
+    uint64 ScheduledToIndex;
+
+    UPROPERTY()
+    EDirection ScheduledDirection;
+
+    UPROPERTY()
+    int32 MoveCount;
 };
